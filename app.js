80a500d03314395bc916dc42ab187b0116e30597
vankai <625608852@qq.com>
2017-04-26 10:55:20 +0800

    test

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var SECRET = require('./secret.json');
+var WechatAPI = require('wechat-api');
 
 var wechat = require('wechat');
 var config = {
   token: SECRET.token,
   appid: SECRET.appid,
-  encodingAESKey: SECRET.encodingAESKey,
-  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+  //encodingAESKey: SECRET.encodingAESKey,
+  checkSignature: flase // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
-
-
-
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -27,56 +25,87 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 
 app.use(express.query());
 app.use('/wechat', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
-  if(message.MsgType == "text"){
-    res.reply({
-      content: '"你发送的是文本消息"',
-      type: 'text'
-    });
+  if (message.MsgType == "text") {
+    var input = message.Content.trim();
+    if(input ==="网址"){
+      res.reply({type: "text", content: 'http://baidu.com'});
+    }else if(input === "天气"){
+       res.reply({type: "text", content: '今天晴转阴'});
+    }else{
+      res.reply({content: '"你发送的是文本消息"', type: 'text'});
+    }
+  } else if (message.MsgType == "image") {
+    res.reply({content: '"你发送的是图片消息"', type: 'text'});
+  } else if (message.MsgType == "voice") {
+    res.reply({content: '"你发送的是语音消息"', type: 'text'});
+  } else if (message.MsgType == 'event') {
+
+    if (message.Event === 'subscribe') {
+      // 用户添加时候的消息
+      res.reply('谢谢添加测试帐号:) ');
+    } else if (message.Event === 'unsubscribe') {
+      res.reply('Bye!');
+    } else if(message.Event ==='click'){
+      if(message.EventKey ==='V1001_TODAY_MUSIC'){
+        // res.reply('谢谢点赞~');
+        res.reply([
+      {
+        title: '北京欢迎你',
+        description: '描述信息',
+        picurl: 'http://wxtest.heitaov.cn/pic1.jpg',
+        url: 'http://baidu.com'
+      }
+    ,
+      {
+        title: '千里之外',
+        description: '描述信息',
+        picurl: 'http://wxtest.heitaov.cn/pic1.jpg',
+        url: 'http://baidu.com'
+      }
+    ])
+      }else if(message.EventKey  ==='V1001_GOOD'){
+          res.reply('谢谢点赞~');
+      }else{
+         res.reply('未知菜单');
+      }
+    }else{
+      res.reply('暂未支持! Coming soon!');
+    }
+
   }
-  else if(message.MsgType == "image"){
-    res.reply({
-      content: '"你发送的是图片消息"',
-      type: 'text'
-    });}
-   
-  else if(message.MsgType == "voice"){
-     res.reply({
-      content: '"你发送的是语音消息"',
-      type: 'text'
-    });
-  }
-  
+
 }));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req
+    .app
+    .get('env') === 'development'
+    ? err
+    : {};
 
   // render the error page
   res.status(err.status || 500);
---
+++ onshot.js
@@ -0,0 +1,29 @@
+var WechatAPI = require('wechat-api');
+var SECRET = require('./secret.json');
+
+var api = new WechatAPI(SECRET.appid, SECRET.appsecret);
+var menu = {
+  "button": [
+    {
+      "type": "click",
+      "name": "今日歌曲",
+      "key": "V1001_TODAY_MUSIC"
+    }, {
+      "name": "菜单",
+      "sub_button": [
+        {
+          "type": "view",
+          "name": "搜索",
+          "url": "http://www.soso.com/"
+        }, {
+          "type": "click",
+          "name": "赞一下我们",
+          "key": "V1001_GOOD"
+        }
+      ]
+    }
+  ]
+};
+api.createMenu(menu, function(err,result){
+  console.log(err,result);
+});
